99b9472120717ddbcd5724ba7e83ed275fac0f33
fatosy <fatosy_code@163.com>
2025-09-16 18:56:23 +0800

    feat: 完成V1版本

--- example/your_project_name/main.py
+++ example/your_project_name/main.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
-    run_kairo("example", 5040, "0.0.0.0")
+    run_kairo("example", 9140, "0.0.0.0")
--- example/your_project_name/schema/user.py
+++ example/your_project_name/schema/user.py
@@ -37,13 +37,17 @@ class UserAddor(BaseModel):
                 raise SCM_USER_PARAM_VALIDATE_ERROR.msg_format("时间参数格式错误，请检查！")
             Ktimer.validate_date_format(v)
         return v
-    
+
+class UserAddtorList(BaseModel):
+    items: List[UserAddor] = Field(description="用户列表")
 
 
 # 用户修改
 class UserUpdator(UserAddor):
     id: int = Field(description="用户id")
 
+class UserUpdatorList(BaseModel):
+    items: List[UserUpdator] = Field(description="用户列表")
 
 
 # 用户删除
@@ -51,11 +55,29 @@ class UserDeletor(BaseModel):
     ids: List[int] = Field(description="用户id")
 
 
+class UserQuery(BaseModel):
+    limit: Optional[int] = Field(default=20, description="查询数量")
+    offset: Optional[int] = Field(default=1, description="页码")
+
 
 # 用户查询
-class UserQuery(BaseModel):
-    id: Optional[int] = Field(default=None, description="用户id")
+class UserQueryBody(BaseModel):
     name: Optional[str] = Field(default=None, description="用户名")
     phone: Optional[str] = Field(default=None, description="手机号")
+    sex: Optional[UserSex] = Field(default=None, description="性别")
     location: Optional[str] = Field(default=None, description="地址")
-    birthday: Optional[str] = Field(default=None, description="生日")
+    birthday_start: Optional[str] = Field(default=None, description="生日起始")
+    birthday_end: Optional[str] = Field(default=None, description="生日结束")
+
+    @field_validator('birthday_start', 'birthday_end')
+    def validate_date_format(cls, v):
+        if v is not None:
+            if not KcReTool.validate_date_format(v):
+                raise SCM_USER_PARAM_VALIDATE_ERROR.msg_format("时间参数格式错误，请检查！")
+            Ktimer.validate_date_format(v)
+        return v
+    
+
+# 用户options
+class UserInfoOptionsGettor(BaseModel):
+    name: Optional[str] = Field(None, description="用户名称")
